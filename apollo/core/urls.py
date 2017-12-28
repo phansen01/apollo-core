7a4bfa8087f47655aa3aa101300618bf3d1baa0f
Preston Hansen <prestonhansen01@gmail.com>
2017-12-28 12:47:54 -0600

    Use generic mixins.

--- apollo/core/urls.py
+++ apollo/core/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from apollo.core import views
 
 urlpatterns = [
-    url(r'^rooms/$', views.room_list)
+    url(r'^rooms/$', views.RoomList.as_view())
 ]
--- apollo/core/views.py
+++ apollo/core/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from apollo.core.serializers import (UserSerializer, GroupSerializer,
     RoomSerializer, RoomDataSerializer)
 from apollo.core.models import Room, RoomData
-
+from apollo.core.permissions import permissions
+from apollo.core.permissions import IsOwnerOrReadOnly
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,19 +25,9 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-@api_view(['GET', 'POST'])
-def room_list(request):
-    """
-    List all rooms or add one
-    """
+class RoomList(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
-    if request.method == 'GET':
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = RoomSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#todo view for RoomData
