6a33f3fb9959940d1edb6733c58a951e741f38cf
Preston Hansen <prestonhansen01@gmail.com>
2018-01-11 19:23:38 -0600

    Add admin route.

--- apollo/core/urls.py
+++ apollo/core/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.urls import path
 from apollo.core import views
 
 urlpatterns = [
     url(r'^api/rooms/$', views.RoomList.as_view()),
     url(r'^api/search/$', views.MatchingRooms.as_view()),
-    path('api/rooms/<int:id>', views.RoomDetail.as_view())
+    path('api/rooms/<int:id>', views.RoomDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
